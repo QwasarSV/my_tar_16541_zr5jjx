4d2717994c53dddf9d6e64894e19cadf7f0e53f8
brendan <brendanrayw@gmail.com>
2021-03-15 01:49:23 -0700

    added error catch for non existent files to prevent seg faulting

--- MyTar_Funcs/loadFromFilenames.c
+++ MyTar_Funcs/loadFromFilenames.c
@@ -1,5 +1,7 @@
 #include "../my_tar.h"
 
+static int	file_exists(char *file);
+
 void	loadFromFilenames(t_MyTar *self, char **filenames, int file_count) {
 	t_MyTarFile	*cur;
 	t_MyTarFile	*prev = NULL;
@@ -8,6 +10,11 @@ void	loadFromFilenames(t_MyTar *self, char **filenames, int file_count) {
 	self->_data = my_tar_file_init();
 	cur = self->_data;
 	while (i < file_count) {
+		if (file_exists(filenames[i]) == -1) {
+			printf("tar: %s: Cannot stat: No such file or directory\n", filenames[i]);
+			i++;
+			continue;
+		}
 		cur->prev = prev;
 		cur->createFromFilename(cur, filenames[i]);
 		if (cur->file_info->typeflag == 5) {
@@ -20,4 +27,10 @@ void	loadFromFilenames(t_MyTar *self, char **filenames, int file_count) {
 		cur = cur->next;
 		i++;
 	}
+}
+
+static int	file_exists(char *file) {
+	struct stat sb;
+
+	return stat(file, &sb);
 }
